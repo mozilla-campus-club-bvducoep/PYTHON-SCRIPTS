ba2370315405c146073f958027206d89606ae458
Siddhant356 <siddhantshukla356@gmail.com>
2020-10-03 03:12:17 +0530

    Adaptive thresholding in opencv

---
+++ adaptive_thresholding.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('data//sudoku.png', 0)
+th1 = cv2.adaptiveThreshold(img,255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+th2 = cv2.adaptiveThreshold(img,255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+cv2.imshow('image', img)
+cv2.imshow('th1', th1)
+cv2.imshow('th2', th2)
+
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
+
