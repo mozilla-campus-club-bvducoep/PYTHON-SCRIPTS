8567989563fdbe13541b8e15514840866e0ab277
KSHITIZMISHRA31 <46749989+KSHITIZMISHRA31@users.noreply.github.com>
2020-10-06 23:30:59 +0530

    updated
    added user input and for loop is also added

--- while.py
+++ while.py
@@ -1,5 +1,12 @@
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+i = int(input("Enter a Number")) #by default all inputs are strings 
+#while Loop iteration
+j=0
+print("output by while loop")
+while j < i:
+  print(j)
+  j =j + 1
+#for Loop iteration
+print("output by for loop")
+for j in range(i):
+  print(j)
 
